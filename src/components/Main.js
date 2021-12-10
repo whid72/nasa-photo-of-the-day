567b06400fe65f92742b2a1c2fb8fd7918e63b2d
Darrion_White <wdarrion64@gmail.com>
2021-12-09 18:44:07 -0600

    added bootstrap and reactstrap dependencies

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState} from "react";
 import axios from "axios";
+import "./Main.css"
 
 
 
@@ -21,7 +22,7 @@ const Main = () => {
             setExp(res.data.explanation);
             setTitle(res.data.title);
         })
-    }, [])
+    }, [url])
 
     return (
     <div className='container'>
